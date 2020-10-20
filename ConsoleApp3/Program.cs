0f5deff218695eed388ddd73b1cd8b498db0b05b
Svante Pålsson <70195788+svante1987@users.noreply.github.com>
2020-10-20 11:26:02 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
